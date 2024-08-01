922694412c5a2da5eae6bf97d91e6cdc1d49de91
Rahul-bista <rahulbista850@gmail.com>
2024-08-01 21:21:24 +0545

    feat: settings in quiz

--- app/src/main/java/com/utsav/worldmapquiz/QuizActivity.java
+++ app/src/main/java/com/utsav/worldmapquiz/QuizActivity.java
@@ -1,10 +1,15 @@
 package com.utsav.worldmapquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QuizActivity extends AppCompatActivity {
+    Button settingsButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,5 +17,15 @@ public class QuizActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_quiz);
+
+        settingsButton = findViewById(R.id.settingsButton);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(QuizActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/utsav/worldmapquiz/SettingsActivity.java
+++ app/src/main/java/com/utsav/worldmapquiz/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.utsav.worldmapquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
